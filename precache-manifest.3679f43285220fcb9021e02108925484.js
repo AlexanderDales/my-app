952326469aee759f116c8a7a4b0647d7b6e3d4a7
alexdales <contact@alexdales.com>
2020-01-30 13:31:40 -0500

    Updates

---
+++ precache-manifest.3679f43285220fcb9021e02108925484.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3290a484e45e9caffe6938e439a8bc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b77f6e41f9aa57e3fa3",
+    "url": "/static/css/main.4d513226.chunk.css"
+  },
+  {
+    "revision": "0900ba7e25030bd225d0",
+    "url": "/static/js/2.938d6267.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.938d6267.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b77f6e41f9aa57e3fa3",
+    "url": "/static/js/main.74d3b042.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "109365bcd17a4fcd72f0dc5891573d95",
+    "url": "/static/media/AlexDales_Resume.109365bc.pdf"
+  },
+  {
+    "revision": "b173381907b592293591b999608ccc2a",
+    "url": "/static/media/ar.b1733819.png"
+  },
+  {
+    "revision": "0b637716b640cfe9fc52fbc671507c52",
+    "url": "/static/media/as.0b637716.png"
+  },
+  {
+    "revision": "da864d3508abf8e78738e420c960bfcd",
+    "url": "/static/media/astralis.da864d35.png"
+  },
+  {
+    "revision": "59ae09e721de4f4dbfb76ef6e1877075",
+    "url": "/static/media/brendan.59ae09e7.png"
+  },
+  {
+    "revision": "c8b111a6b1e24e75ce0a805cb63fc89a",
+    "url": "/static/media/ceye.c8b111a6.png"
+  },
+  {
+    "revision": "e4843f3d3f73ff8639385c43ecfa4c4d",
+    "url": "/static/media/crayon.e4843f3d.png"
+  },
+  {
+    "revision": "a5e0fbb5de608fa6974c230ecc7a74ce",
+    "url": "/static/media/csgocrave.a5e0fbb5.png"
+  },
+  {
+    "revision": "6ec25c8f87db85774a6c40ef43d10397",
+    "url": "/static/media/dailysum.6ec25c8f.png"
+  },
+  {
+    "revision": "32daf5842eb3b1461ee4e3d3b4e49b61",
+    "url": "/static/media/dna.32daf584.png"
+  },
+  {
+    "revision": "e2798590e71a9bab2d53ae52630ee2fa",
+    "url": "/static/media/dribbble.e2798590.svg"
+  },
+  {
+    "revision": "d7cd64f6313423c12503c749d25b64a9",
+    "url": "/static/media/flower.d7cd64f6.png"
+  },
+  {
+    "revision": "1f2c47dbb8af9e0c6b14c0616a98b621",
+    "url": "/static/media/instagram.1f2c47db.svg"
+  },
+  {
+    "revision": "021295e455bf54cfc2d9f4716734c4a8",
+    "url": "/static/media/interactivehome.021295e4.png"
+  },
+  {
+    "revision": "64f0dcdd567c56b93ea0ca3b08786c44",
+    "url": "/static/media/jf.64f0dcdd.png"
+  },
+  {
+    "revision": "3e2cd0696475825581f4d357467f9bed",
+    "url": "/static/media/linkedin.3e2cd069.svg"
+  },
+  {
+    "revision": "534a4aac2a419cfb529cdc76069649c8",
+    "url": "/static/media/m6.534a4aac.png"
+  },
+  {
+    "revision": "4bd6af1d65ee606d77ab2b5e1b19afb7",
+    "url": "/static/media/mored.4bd6af1d.png"
+  },
+  {
+    "revision": "2438e7718bbacc64b9eacb4b0dc02536",
+    "url": "/static/media/nikko.2438e771.png"
+  },
+  {
+    "revision": "298d02c7c0b26c74cd97b43bdda7276f",
+    "url": "/static/media/personalLogo.298d02c7.svg"
+  },
+  {
+    "revision": "1d8fed7e5b325d5c0bff46d8e4985025",
+    "url": "/static/media/swift.1d8fed7e.png"
+  },
+  {
+    "revision": "6c199d796f8c6dc7ab1f256eb2f55bc1",
+    "url": "/static/media/twc.6c199d79.png"
+  },
+  {
+    "revision": "bb8fdc991369722ac519dbb29f223e67",
+    "url": "/static/media/twitter.bb8fdc99.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.522a3b166853aa4fe9eafe6c3ea99c7b.js"
+  "/precache-manifest.3679f43285220fcb9021e02108925484.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
